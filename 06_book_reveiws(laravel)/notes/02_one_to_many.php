d7e17e52e5f65a67854dd322373e88772c414887
aliabdulbaqi01 <aliabdulbaqi01@gmail.com>
2024-10-05 00:55:55 +0300

    06_book_review(laravel) : book and review entities (model and maigration)

---
+++ 06_book_reveiws(laravel)/notes/02_one_to_many.php
@@ -0,0 +1,36 @@
+<?php
+// one to many
+/*
+ * in one-to-many relationship
+ * one child belongs to one parent
+ * one parent has many children
+ */
+
+
+//----------------------------
+// migration
+/*
+ * we have multiple ways
+ * first
+ *  - $table->unsignedBigInteger('book_id')
+ *  - $table->foreign('book_id')->reference('id')->on('books')->ondelete('cascade')
+ *
+ * second
+ *  - $table->foreignId('book_id')->constrained()->cascadeondelete();
+ */
+
+
+//------------------------------------
+// model
+/*
+ * in parent model (Book)
+ * (function name should be plural)
+ *      - public function  reviews() {
+ *          return $this->hasMany(Review::class)
+ *        }
+ *
+ * in child class (Review
+ *      - public function bood() {
+ *          return $this->belongsTo(Books::class);
+ *        }
+ */
